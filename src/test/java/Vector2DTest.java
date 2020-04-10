00675112de747d2235fe5e7ca17dcf64e2d823b9
cat-inGlasses <nikita.pochapynskyi@gmail.com>
2020-04-10 10:29:15 +0200

    JUnit testing, using annotations

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,27 +1,33 @@
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
 
+    private final double EPS = 1e-9;
+    private static Vector2D v1;
+
+    @BeforeClass
+    public static void createNewVector() {
+        v1 = new Vector2D();
+    }
+
     @Test
     public void newVectorShouldHaveZeroLength() {
-        // action
-        Vector2D v1 = new Vector2D();
-
         // assertion
         // 1e-9 == 0.0000000001
-        Assert.assertEquals(0, v1.length(), 1e-9);
+        Assert.assertEquals(0, v1.length(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroX() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getX(), 1e-9);
+        Assert.assertEquals(0, v1.getX(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroY() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getY(), 1e-9);
+        Assert.assertEquals(0, v1.getY(), EPS);
     }
+
 }
